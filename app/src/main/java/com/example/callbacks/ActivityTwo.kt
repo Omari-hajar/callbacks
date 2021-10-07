e53486e198431219dace88d88500070a3e5c4394
Omari-hajar <89646158+Omari-hajar@users.noreply.github.com>
2021-10-07 09:20:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/callbacks/ActivityTwo.kt
@@ -0,0 +1,71 @@
+package com.example.callbacks
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.Toast
+
+class ActivityTwo : AppCompatActivity() {
+    lateinit var btnFirst: Button
+    lateinit var btnTwo: Button
+    lateinit var btnThree: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_two)
+
+        btnFirst = findViewById(R.id.btnFirst)
+        btnTwo = findViewById(R.id.btnTwo)
+        btnThree = findViewById(R.id.btnThree)
+
+
+        btnFirst.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        btnTwo.setOnClickListener {
+            val intent = Intent(this, ActivityTwo::class.java)
+            startActivity(intent)
+        }
+        btnThree.setOnClickListener {
+            val intent = Intent(this, ActivityThree::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        Toast.makeText(this, "This is onCreate", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "This is onCreate", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "This is onStart", Toast.LENGTH_LONG).show()
+
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "This is onPause", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "This is onStop", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "This is onDestroy", Toast.LENGTH_LONG).show()
+    }
+}
